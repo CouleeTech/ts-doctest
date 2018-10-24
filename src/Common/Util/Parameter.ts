718fefba0f4db468c631c08549b9c1aec11a3e73
William McCullough <will.mccullough@couleetechlink.com>
2018-10-23 23:23:22 -0500

    dev - Created a function that builds label-style parameter strings.

--- src/Common/Util/Parameter.ts
+++ src/Common/Util/Parameter.ts
@@ -2,13 +2,84 @@ import { IsObject, IsArray, IsString, IsNumber, IsFalsy } from '../Validation'
 
 export type ParameterString = string
 
+const INVALID_PARAMETER_VALUE = 'Invalid parameter string value'
+
+/**
+ * Build a form-style parameter string
+ *
+ * This format is defined by RFC6570.
+ *
+ * @param name The name of the parameter.
+ * @param value The value of the parameter.
+ * @param explode Build the parameter string in an expanded format.
+ */
+export function FormParameterString(name: string, value: any, explode: boolean = true) {
+  if (IsFalsy(value)) {
+    return `${name}=`
+  } else if (IsString(value) || IsNumber(value)) {
+    return `${name}=${value}`
+  } else if (IsArray(value)) {
+    if (explode) {
+      return `${value.map((entry: any) => `${name}=${entry}`).join('&')}`
+    }
+
+    return `${name}=${value.join(',')}`
+  } else if (IsObject(value)) {
+    if (explode) {
+      return `${Object.keys(value)
+        .map(key => `${key}=${value[key]}`)
+        .join('&')}`
+    }
+
+    const valueString = Object.keys(value)
+      .map(key => [key, value[key]])
+      .join(',')
+
+    return `${name}=${valueString}`
+  }
+
+  throw new Error(INVALID_PARAMETER_VALUE)
+}
+
+/**
+ * Build a label-style parameter string
+ *
+ * This format is defined by RFC6570.
+ *
+ * @param value The value of the parameter.
+ * @param explode Build the parameter string in an expanded format.
+ */
+export function LabelParameterString(value: any, explode: boolean = false) {
+  if (IsFalsy(value)) {
+    return `.`
+  } else if (IsString(value) || IsNumber(value)) {
+    return `.${value}`
+  } else if (IsArray(value)) {
+    return `.${value.join('.')}`
+  } else if (IsObject(value)) {
+    if (explode) {
+      return `.${Object.keys(value)
+        .map(key => `${key}=${value[key]}`)
+        .join('.')}`
+    }
+
+    const valueString = Object.keys(value)
+      .map(key => `${key}.${value[key]}`)
+      .join('.')
+
+    return `.${valueString}`
+  }
+
+  throw new Error(INVALID_PARAMETER_VALUE)
+}
+
 /**
  * Build a matrix-style parameter string
  *
  * This format is defined by RFC6570.
  *
- * @param name The name of the parameter parameter
- * @param value The value of the parameter parameter.
+ * @param name The name of the parameter.
+ * @param value The value of the parameter.
  * @param explode Build the parameter string in an expanded format.
  */
 export function MatrixParameterString(name: string, value: any, explode: boolean = false): ParameterString {
@@ -36,42 +107,5 @@ export function MatrixParameterString(name: string, value: any, explode: boolean
     return `;${name}=${valueString}`
   }
 
-  throw new Error('Invalid parameter string value')
-}
-
-/**
- * Build a form-style parameter string
- *
- * This format is defined by RFC6570.
- *
- * @param name The name of the parameter parameter
- * @param value The value of the parameter parameter.
- * @param explode Build the parameter parameter in an expanded format.
- */
-export function FormParameterString(name: string, value: any, explode: boolean = true) {
-  if (IsFalsy(value)) {
-    return `${name}=`
-  } else if (IsString(value) || IsNumber(value)) {
-    return `${name}=${value}`
-  } else if (IsArray(value)) {
-    if (explode) {
-      return `${value.map((entry: any) => `${name}=${entry}`).join('&')}`
-    }
-
-    return `${name}=${value.join(',')}`
-  } else if (IsObject(value)) {
-    if (explode) {
-      return `${Object.keys(value)
-        .map(key => `${key}=${value[key]}`)
-        .join('&')}`
-    }
-
-    const valueString = Object.keys(value)
-      .map(key => [key, value[key]])
-      .join(',')
-
-    return `${name}=${valueString}`
-  }
-
-  throw new Error('Invalid parameter string value')
+  throw new Error(INVALID_PARAMETER_VALUE)
 }
--- src/__tests__/Common/Parameter.spec.ts
+++ src/__tests__/Common/Parameter.spec.ts
@@ -1,4 +1,4 @@
-import { MatrixParameterString, FormParameterString } from '../../Common/Util/Parameter'
+import { MatrixParameterString, FormParameterString, LabelParameterString } from '../../Common/Util/Parameter'
 
 const testName = 'color'
 const emptyValue: any = false
@@ -7,31 +7,6 @@ const testArrayValue = ['blue', 'black', 'brown']
 const testObjectValue = { R: 100, G: 200, B: 150 }
 
 describe('Parameter Building Functions', () => {
-  it('Should include include a function that creates matrix-style parameters.', () => {
-    const emptyResult = ';color'
-
-    expect(MatrixParameterString(testName, emptyValue)).toBe(emptyResult)
-    expect(MatrixParameterString(testName, emptyValue, true)).toBe(emptyResult)
-
-    const stringResult = ';color=blue'
-    const explodedStringResult = ';color=blue'
-
-    expect(MatrixParameterString(testName, testStringValue)).toBe(stringResult)
-    expect(MatrixParameterString(testName, testStringValue, true)).toBe(explodedStringResult)
-
-    const arrayString = ';color=blue,black,brown'
-    const explodedArrayString = ';color=blue;color=black;color=brown'
-
-    expect(MatrixParameterString(testName, testArrayValue)).toBe(arrayString)
-    expect(MatrixParameterString(testName, testArrayValue, true)).toBe(explodedArrayString)
-
-    const objectString = ';color=R,100,G,200,B,150'
-    const explodedObjectString = ';R=100;G=200;B=150'
-
-    expect(MatrixParameterString(testName, testObjectValue)).toBe(objectString)
-    expect(MatrixParameterString(testName, testObjectValue, true)).toBe(explodedObjectString)
-  })
-
   it('Should include include a function that creates form-style parameters.', () => {
     const emptyResult = 'color='
 
@@ -55,5 +30,55 @@ describe('Parameter Building Functions', () => {
 
     expect(FormParameterString(testName, testObjectValue, false)).toBe(objectString)
     expect(FormParameterString(testName, testObjectValue)).toBe(explodedObjectString)
+  })
+
+  it('Should include include a function that creates label-style parameters.', () => {
+    const emptyResult = '.'
+
+    expect(LabelParameterString(emptyValue)).toBe(emptyResult)
+    expect(LabelParameterString(emptyValue, true)).toBe(emptyResult)
+
+    const stringResult = '.blue'
+    const explodedStringResult = '.blue'
+
+    expect(LabelParameterString(testStringValue)).toBe(stringResult)
+    expect(LabelParameterString(testStringValue, true)).toBe(explodedStringResult)
+
+    const arrayString = '.blue.black.brown'
+    const explodedArrayString = '.blue.black.brown'
+
+    expect(LabelParameterString(testArrayValue)).toBe(arrayString)
+    expect(LabelParameterString(testArrayValue, true)).toBe(explodedArrayString)
+
+    const objectString = '.R.100.G.200.B.150'
+    const explodedObjectString = '.R=100.G=200.B=150'
+
+    expect(LabelParameterString(testObjectValue)).toBe(objectString)
+    expect(LabelParameterString(testObjectValue, true)).toBe(explodedObjectString)
+  })
+
+  it('Should include include a function that creates matrix-style parameters.', () => {
+    const emptyResult = ';color'
+
+    expect(MatrixParameterString(testName, emptyValue)).toBe(emptyResult)
+    expect(MatrixParameterString(testName, emptyValue, true)).toBe(emptyResult)
+
+    const stringResult = ';color=blue'
+    const explodedStringResult = ';color=blue'
+
+    expect(MatrixParameterString(testName, testStringValue)).toBe(stringResult)
+    expect(MatrixParameterString(testName, testStringValue, true)).toBe(explodedStringResult)
+
+    const arrayString = ';color=blue,black,brown'
+    const explodedArrayString = ';color=blue;color=black;color=brown'
+
+    expect(MatrixParameterString(testName, testArrayValue)).toBe(arrayString)
+    expect(MatrixParameterString(testName, testArrayValue, true)).toBe(explodedArrayString)
+
+    const objectString = ';color=R,100,G,200,B,150'
+    const explodedObjectString = ';R=100;G=200;B=150'
+
+    expect(MatrixParameterString(testName, testObjectValue)).toBe(objectString)
+    expect(MatrixParameterString(testName, testObjectValue, true)).toBe(explodedObjectString)
   })
 })
